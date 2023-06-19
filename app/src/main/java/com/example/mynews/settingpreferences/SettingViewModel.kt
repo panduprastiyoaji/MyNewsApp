86b2ba56aac22d9d934e01959d43ba5b93f0cfdf
Pandu <panduprastiyoaji@gmail.com>
2023-06-19 18:22:48 +0700

    update

--- app/src/main/java/com/example/mynews/settingpreferences/SettingViewModel.kt
+++ app/src/main/java/com/example/mynews/settingpreferences/SettingViewModel.kt
@@ -10,6 +10,7 @@ class SettingViewModel(private val pref: SettingPreferences) : ViewModel() {
     fun getThemeSettings(): LiveData<Boolean> {
         return pref.getThemeSetting().asLiveData()
     }
+
     fun saveThemeSetting(isDarkModeActive: Boolean) {
         viewModelScope.launch {
             pref.saveThemeSetting(isDarkModeActive)
